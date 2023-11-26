3e193acb7f84922332d05ac0749e9a56d794cf7e
unsighera <eddy.weston@bk.ru>
2023-11-26 23:56:22 +0300

    Добавьте файлы проекта.

---
+++ Battleship/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Battleship;
+
+var User = new User();
+var Bot = new Bot();
+bool Continue = true;
+while (Continue)
+{
+    while (true)
+    {
+        User.Output(User.Field1);
+        User.Strike();
+        if (User.Win()) { break; }
+        Bot.Strike();
+        if (Bot.Win()) { break; }
+    }
+    Console.SetCursorPosition(30, 1);
+    Console.WriteLine("Спасибо за игру! Хотите сыграть еще раз? ");
+    if (Console.ReadLine() != "да")
+    {
+        Console.Clear();
+        Continue = false;
+    }
+}
